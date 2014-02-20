afa6444af2f59b1d1a4ba333b4ee53cd7b898a68
xifengzhu <xifengzhu520@gmail.com>
2014-02-21 00:06:52 +0800

    add bootstrap3 and use mongo

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) { res.render('index', { title: 'Express' });
 };
-
-exports.hello = function(req, res){
-	res.send('the time is '+ new Date().toString());
-}
+exports.user = function(req, res) { };
+exports.post = function(req, res) { };
+exports.reg = function(req, res) { };
+exports.doReg = function(req, res) { };
+exports.login = function(req, res) { };
+exports.doLogin = function(req, res) { };
+exports.logout = function(req, res) { };
